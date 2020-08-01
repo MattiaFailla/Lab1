a0f21d0f1b3b8e8a38fc2df52e28cfcd57d1eefd
Mattia <mattiafailla@hotmail.it>
2020-08-01 12:24:12 +0200

    Pushing Ristoratori

---
+++ ristoratori/src/com/registrazione/App.java
@@ -0,0 +1,73 @@
+package com.registrazione;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton loginButton;
+    private JPanel panel1;
+    private JButton registrazioneButton;
+    private JPasswordField passwordField1;
+    private JTextField textField1;
+
+    public App() {
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Login phase
+                JOptionPane.showMessageDialog(null, "Bottone click login");
+            }
+        });
+        registrazioneButton.addActionListener(new ActionListener() {
+            /**
+             * Invoked when an action occurs.
+             *
+             * @param e the event to be processed
+             */
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Subscription page
+                JOptionPane.showMessageDialog(null, "Apertura pagina di registrazione.");
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        panel1 = new JPanel();
+        loginButton = new JButton();
+    }
+
+
+    public static void main(String[] args){
+
+        JFrame frame;
+        frame = new JFrame("Appliczione");
+
+        frame.setSize(400, 600);
+
+        Container app = new App().panel1;
+        frame.setContentPane(app);
+
+
+        //frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+        frame.setVisible(true);
+
+        /*JFrame frame;
+        frame = new JFrame();
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setTitle("Frytree");
+
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);*/
+    }
+
+}
