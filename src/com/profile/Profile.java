c513859646117b4b410ca66d27e0027ae7dbae50
Samuel <samuelpedotti92@gmail.com>
2020-08-03 18:13:15 +0200

    Debugging Profile

---
+++ src/com/profile/Profile.java
@@ -0,0 +1,40 @@
+package com.profile;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Profile {
+    private JPanel panel2;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JPasswordField passwordField1;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField fullNameTextField;
+    private JTextField userFirstNameTextField;
+    private JButton saveButton;
+
+    public Profile() {
+        saveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, "Saved!");
+            }
+        });
+    }
+
+    /*private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }*/
+
+    public static void main(String[] args) {
+        Jframe frame = new Jframe("Profile");
+        frame.setContentPane(new Profile().panel2);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
