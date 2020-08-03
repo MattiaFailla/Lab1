29c1c7a72963c3f18459966df61677eaca772629
Mattia <mattiafailla@hotmail.it>
2020-08-03 19:34:52 +0200

    Improving registration

--- src/com/login/Registration.java
+++ src/com/login/Registration.java
@@ -4,23 +4,31 @@ import com.profile.Profile;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
 public class Registration {
-    private JButton registramiNelSistemaButton;
     private JPanel panel1;
+    private JTextField marioRossiTextField;
+    private JPasswordField passwordField1;
+    private JButton registramiEInserisciInformazioniButton;
 
     public static void main(String[] args) throws IOException {
-        JFrame frame = new JFrame("Registrati") {
-            private Image backgroundImage = ImageIO.read(new File("/home/mat/Progetti/Lab1/assets/img/login_background.jpg"));
+        // @fixme: This need to be fixed. The drawImage show some strange behaviour with the content of the frame
+        
+        JFrame frame = new JFrame("Registrazione Ristoratori") {
+            private final Image backgroundImage = ImageIO.read(new File("/home/mat/Progetti/Lab1/assets/img/login_background.jpg"));
             public void paint( Graphics g ) {
                 super.paint(g);
-                g.drawImage(backgroundImage, 0, 0, null);
+                // Setting the background with custom dimensions
+                g.drawImage(backgroundImage, 0, 0, 860, 540, this);
             }
         };
-        frame.setSize(600,800);
+        //JFrame frame = new JFrame();
+        frame.setPreferredSize(new Dimension(860,540));
         frame.setContentPane(new Registration().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
