3df8af96455594e8dcb0762e0efceda51b450c9c
Mattia <mattiafailla@hotmail.it>
2020-08-05 15:58:18 +0200

    Adding code to create the database

--- src/StartApp.java
+++ src/StartApp.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class StartApp {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("App started");
+
+
+        // Creating database if not exists
+        File db_ristoratori = new File("./risotaratori.db");
+        // Forcing the os to create the files
+        db_ristoratori.createNewFile();
+        FileOutputStream oFile = new FileOutputStream(db_ristoratori, false);
+
+        // Starting the ui
         com.login.Login.main(args);
+
     }
 }
