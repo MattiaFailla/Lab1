2d1815ec43af5cb397ba36a0de16e98b2b875b05
Mattia <mattiafailla@hotmail.it>
2020-08-05 16:05:45 +0200

    Calling db.init() from StartApp.java

--- src/StartApp.java
+++ src/StartApp.java
@@ -1,9 +1,12 @@
+import com.DataBase;
+
 import java.io.IOException;
 
 public class StartApp {
     public static void main(String[] args) throws IOException {
         System.out.println("App started");
 
+        DataBase.init();
 
         // Starting the ui
         com.login.Login.main(args);
