b6275e2118878db5ee377038227d219259f0d7a3
Mattia <mattiafailla@hotmail.it>
2020-08-05 16:59:15 +0200

    Structuring database write
    Base structure for the database

--- src/StartApp.java
+++ src/StartApp.java
@@ -1,12 +1,15 @@
 import com.DataBase;
 
 import java.io.IOException;
+import java.sql.DatabaseMetaData;
 
 public class StartApp {
     public static void main(String[] args) throws IOException {
         System.out.println("App started");
 
         DataBase.init();
+
+        DataBase.write(1, DataBase.data_types.INFO, "App started");
 
         // Starting the ui
         com.login.Login.main(args);
--- src/com/DataBase.java
+++ src/com/DataBase.java
@@ -1,10 +1,19 @@
 package com;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class DataBase {
     private static final String filename = "./database.db";
+
+    public enum data_types {
+        INFO,
+        LOGGING,
+        USER_DATA,
+        INTERNAL_DATA
+    }
 
     public static void init() throws IOException {
         // Initialize the database
@@ -12,6 +21,17 @@ public class DataBase {
         File db_ristoratori = new File(filename);
         // Forcing the os to create the files
         db_ristoratori.createNewFile();
+    }
+
+    public static void write(Integer id, data_types type, String content) throws IOException {
+        // Appending to file the content at id
+        FileOutputStream fos = new FileOutputStream(filename, true);
+        String payload = "# DATA" + "\n" +
+                "id:" + id.toString() + "\n" +
+                "type:" + type + "\n" +
+                "content:" + content + "\n";
+        fos.write(payload.getBytes());
+        fos.close();
     }
 
 
