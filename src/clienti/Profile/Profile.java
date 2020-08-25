d94d96b77444b1aeb4efdc65d0489974226891d3
Samuel <samuelpedotti92@gmail.com>
2020-08-25 18:57:44 +0200

    Improving Profile input validation

--- src/clienti/Profile/Profile.java
+++ src/clienti/Profile/Profile.java
@@ -1,6 +1,8 @@
 package clienti.Profile;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.awt.event.*;
 
 public class Profile extends JDialog {
@@ -12,7 +14,7 @@ public class Profile extends JDialog {
     private JComboBox restaurantComboBox;
     private JButton saveButton;
     private JButton cancelButton;
-    private JTextField textField1;
+    private JTextField NicknameField;
 
     public Profile() {
         setContentPane(contentPane);
@@ -34,7 +36,57 @@ public class Profile extends JDialog {
             public void windowClosing(WindowEvent e) { onCancel(); }
         });
         // endregion
+
+        NicknameField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { NicknameField.selectAll(); }
+            public void focusLost(FocusEvent e) {
+                boolean validate = notValidateNick();
+                if(!validate) {
+                    NicknameField.setBorder(new LineBorder(Color.green));
+                    onEnabled();
+                }
+                else { NicknameField.setBorder(new LineBorder(Color.red)); }
+            }
+        });
+        passwordField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { passwordField.selectAll(); }
+            public void focusLost(FocusEvent e) {
+                boolean validate = notValidatePass();
+                if(!validate) { passwordField.setBorder(new LineBorder(Color.green));
+                    onEnabled();
+                }
+                else { passwordField.setBorder(new LineBorder(Color.red)); }
+            }
+        });
+        emailField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { emailField.selectAll(); }
+            public void focusLost(FocusEvent e) {
+                boolean validate = notValidateEmail();
+                if(!validate) { emailField.setBorder(new LineBorder(Color.green));
+                    onEnabled();
+                }
+                else { emailField.setBorder(new LineBorder(Color.red)); }}
+        });
+        fullNameField.addFocusListener(new FocusListener() {
+            public void focusGained(FocusEvent e) { fullNameField.selectAll(); }
+            public void focusLost(FocusEvent e) {
+                boolean validate = notValidateFname();
+                if(!validate) { fullNameField.setBorder(new LineBorder(Color.green));
+                    onEnabled();
+                }
+                else { fullNameField.setBorder(new LineBorder(Color.red)); }}
+        });
+
     }
+
+    private boolean notValidateNick() { return NicknameField.getText().matches("\\W+|\\d+"); }
+
+    private boolean notValidatePass() { return String.valueOf(passwordField.getPassword()).matches("^(.{0,7}|[^0-9]*|[^A-Z]*|[^a-z]*|[a-zA-Z0-9]*)$"); }
+
+    private boolean notValidateEmail() { return emailField.getText().matches("\\W+|\\d+"); } // regex must be changed
+
+    private boolean notValidateFname() { return NicknameField.getText().matches("\\W+|\\d+"); }
+
 
     public void WriteInfo() {
     }
@@ -42,6 +94,16 @@ public class Profile extends JDialog {
     private void onCancel() {
         System.out.println("Closing app..");
         dispose();
+    }
+
+    private void onEnabled() {
+        boolean checkName = NicknameField.getText().length() > 0;
+        boolean checkFullName = fullNameField.getText().length() > 0;
+        boolean checkEmail = emailField.getText().length() > 0;
+        boolean checkPassword = String.valueOf(passwordField.getPassword()).length() > 0;
+        boolean firstCouple = checkName && checkFullName;
+        boolean secondCouple = checkEmail && checkPassword;
+        saveButton.setEnabled(firstCouple && secondCouple);
     }
 
     public static void main(String[] args) {
