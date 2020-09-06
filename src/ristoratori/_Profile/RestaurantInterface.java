1aa0d2c5e5418396a9c1ae5dc92ba0d2c17ee0e0
Samuel <samuelpedotti92@gmail.com>
2020-09-06 17:32:16 +0200

    Adding RestaurantInterface

---
+++ src/ristoratori/_Profile/RestaurantInterface.java
@@ -0,0 +1,11 @@
+package ristoratori._Profile;
+
+import _database.objects.Judgement;
+
+import javax.swing.*;
+
+public class RestaurantInterface {
+    private JTextArea Judgements;
+    private JLabel restaurantName;
+    
+}
