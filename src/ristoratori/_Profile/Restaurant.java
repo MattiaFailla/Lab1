c45c6ca29c166ac2f1771f7514dc41b752978c77
Lorenzo <ldangelo1@studenti.uninsubria.it>
2020-09-19 04:46:35 +0200

    Add Restaurant profile

---
+++ src/ristoratori/_Profile/Restaurant.java
@@ -0,0 +1,57 @@
+package ristoratori._Profile;
+
+import _database.Database;
+import _database.DatabaseExceptions;
+import _database.objects.Judgement;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+import java.util.List;
+
+public class Restaurant extends JDialog {
+	private JPanel contentPane;
+	private JLabel restaurantLabel;
+	private JTextArea judgmentArea;
+	private JComboBox starsComboBox;
+	private JTextField judgmentField;
+	public static Restaurant rst;
+	public static boolean verifyClient;
+
+	public Restaurant() {
+		restaurantLabel.setText("collegamento");
+		setContentPane(contentPane);
+		setModal(true);
+
+		//region closing app events
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) { dispose(); }
+		});
+		contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
+		//endregion
+
+		try {
+			List<Judgement> list = Database.getJudgement(restaurantLabel.getText());
+			judgmentArea.append(list.toString());
+		}
+		catch (IOException | ClassNotFoundException e) { e.printStackTrace(); }
+		catch (DatabaseExceptions databaseExceptions) {
+			JOptionPane.showMessageDialog(null, "Restaurant not found");
+		}
+
+		judgmentField.setEditable(verifyClient);
+		starsComboBox.setEnabled(verifyClient);
+	}
+
+	public static void main() {
+		Restaurant dialog = new Restaurant();
+		dialog.pack();
+		dialog.setResizable(false);
+		dialog.setLocationRelativeTo(null);
+		dialog.setTitle("Restaurant - Profile");
+		dialog.setVisible(true);
+	}
+}
