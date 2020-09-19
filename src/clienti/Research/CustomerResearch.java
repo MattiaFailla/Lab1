4513d50b218d6bb4308002f9a7b5b09c294b52d5
Lorenzo <ldangelo1@studenti.uninsubria.it>
2020-09-20 00:47:06 +0200

    Fix the research app, plus add one user

--- src/clienti/Research/CustomerResearch.java
+++ src/clienti/Research/CustomerResearch.java
@@ -14,55 +14,48 @@ public class CustomerResearch extends JDialog {
 	private JPanel contentPane;
 	private JTextField nameField;
 	private JTextField cityField;
-	private JComboBox typologyBox;
+	private JComboBox<String> typologyBox;
 	private JTextArea searchArea;
 	private JButton researchButton;
 	private JButton registrationButton;
-	private JButton loginButton;
+	public static JButton loginButton;
 
 	public CustomerResearch() {
 		setContentPane(contentPane);
 		setModal(true);
 
-		//region cancelButton events
-		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		//region setBorder to Color.red
+		nameField.setBorder(new LineBorder(Color.red));
+		cityField.setBorder(new LineBorder(Color.red));
+
+		typologyBox.setSelectedIndex(-1);
+		//endregion
+
+		//region closing app events
+		setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) { dispose(); }
 		});
-		//contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
+		contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
 		//endregion
 
-		//region switch windows
-		registrationButton.addActionListener(e -> {
-			addComponentListener(new ComponentAdapter() {
-				@Override
-				public void componentHidden(ComponentEvent e) {
-					super.componentHidden(e);
-				}
-			});
-			CustomerRegistration.main();
+		//region researchButton events
+		researchButton.addActionListener(e -> {
+			String name = this.nameField.getText();
+			String city = this.cityField.getText();
+			String typeStr = String.valueOf(this.typologyBox.getSelectedItem());
+			Restaurant.types type = null;
+			if (typeStr != null) { type = Restaurant.types.valueOf(typeStr.toUpperCase()); }
+
+			//Database.getRestaurantByCategory(type);
+
 		});
+		//endregion
+
+		//region Switch windows
+		registrationButton.addActionListener(e -> CustomerRegistration.main());
 		loginButton.addActionListener(e -> CustomerLogin.main());
 		//endregion
-
-		researchButton.addMouseListener(new MouseListener() {
-			public void mouseClicked(MouseEvent e) {
-				String name = nameField.getText();
-				String city = cityField.getText();
-				Restaurant.types types = (Restaurant.types) typologyBox.getSelectedItem();
-
-				//todo: request to database
-
-				//for(Restaurant rst : Database.getRestaurantByName(name)) {
-
-				//}
-				searchArea.append("ristoranti");
-			}
-			public void mousePressed(MouseEvent e) { }
-			public void mouseReleased(MouseEvent e) { }
-			public void mouseEntered(MouseEvent e) { }
-			public void mouseExited(MouseEvent e) { }
-		});
 
 		//region Focus Events
 		nameField.addFocusListener(new FocusListener() {
@@ -88,6 +81,9 @@ public class CustomerResearch extends JDialog {
 	public static void main() {
 		CustomerResearch dialog = new CustomerResearch();
 		dialog.pack();
+		dialog.setResizable(false);
+		dialog.setLocationRelativeTo(null);
+		dialog.setTitle("Research");
 		dialog.setVisible(true);
 	}
 }
