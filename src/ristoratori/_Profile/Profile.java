53beba2d9c567b3d4517ab023b692482a8117349
Lorenzo <ldangelo1@studenti.uninsubria.it>
2020-09-19 19:41:40 +0200

    Save new info for eat advisor

--- src/ristoratori/_Profile/Profile.java
+++ src/ristoratori/_Profile/Profile.java
@@ -33,27 +33,31 @@ public class Profile extends JDialog {
 		contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
 		//endregion
 
+		//region saveButton events
 		saveButton.addMouseListener(new MouseListener() {
-			public void mouseClicked(MouseEvent e) {
-				// Getting data from the form
-				String password = String.valueOf(passwordField.getPassword());
-				String email = emailField.getText();
-				String fullname = fullNameField.getText();
-
-				//regex to split fullname in name and surname
-				Pattern pattern = Pattern.compile("^([a-zA-Z]+)\\s(['a-zA-Z]+)$");
-				Matcher matcher = pattern.matcher(fullname);
-				String name = matcher.group(1);
-				String surname = matcher.group(2);
-
-				Database.insertClient(name, surname, clt.city, clt.province, email, clt.nickname, password);
-				JOptionPane.showMessageDialog(null, "Saved!");
-			}
+			public void mouseClicked(MouseEvent e) { }
 			public void mousePressed(MouseEvent e) { }
 			public void mouseReleased(MouseEvent e) { }
 			public void mouseEntered(MouseEvent e) { }
 			public void mouseExited(MouseEvent e) { }
 		});
+		saveButton.addActionListener(e -> {
+			// Getting data from the form
+			String password = String.valueOf(passwordField.getPassword());
+			String email = emailField.getText();
+			String fullname = fullNameField.getText();
+
+			//regex to split fullname in name and surname
+			Pattern pattern = Pattern.compile("^([a-zA-Z]+)\\s(['a-zA-Z]+)$");
+			Matcher matcher = pattern.matcher(fullname);
+			String name = matcher.group(1);
+			String surname = matcher.group(2);
+
+			// Saving the new eat advisor's info in the database
+			Database.insertClient(name, surname, clt.city, clt.province, email, clt.nickname, password);
+			JOptionPane.showMessageDialog(null, "Saved!");
+		});
+		//endregion
 
 		//region Focus events
 		passwordField.addFocusListener(new FocusListener() {
