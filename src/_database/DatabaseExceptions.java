ac9264c3cc39444ce5a0dd109c4589a2639ad0d0
Mattia <mattiafailla@hotmail.it>
2020-09-20 12:41:46 +0200

    Adding JavaDoc for DatabaseExceptions.java
    Signed-off-by: Mattia <mattiafailla@hotmail.it>

--- src/_database/DatabaseExceptions.java
+++ src/_database/DatabaseExceptions.java
@@ -1,5 +1,8 @@
 package _database;
 
+/**
+ * This class is responsible for managing custom database errors
+ */
 public class DatabaseExceptions extends Throwable {
 
     private static final long serialVersionUID = 8534172284432314098L;
