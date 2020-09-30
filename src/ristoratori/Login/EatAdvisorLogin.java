7fe7e159c550aa3c7144f7bb62eb23a642eb6734
Lorenzo <ldangelo1@studenti.uninsubria.it>
2020-09-30 09:17:19 +0200

    Compact dbException

--- src/ristoratori/Login/EatAdvisorLogin.java
+++ src/ristoratori/Login/EatAdvisorLogin.java
@@ -13,49 +13,49 @@ import java.awt.event.*;
 import java.io.IOException;
 
 public class EatAdvisorLogin extends JDialog {
-    private JPanel contentPane;
-    private JTextField nicknameField;
-    private JPasswordField passwordField;
-    private JButton registerButton;
-    private JButton loginButton;
+	private JPanel contentPane;
+	private JTextField nicknameField;
+	private JPasswordField passwordField;
+	private JButton registerButton;
+	private JButton loginButton;
 
-    public EatAdvisorLogin() {
-        setContentPane(contentPane);
-        setModal(true);
+	public EatAdvisorLogin() {
+		setContentPane(contentPane);
+		setModal(true);
 
-        //region closing app events
-        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
-        contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
-        //endregion
+		//region closing app events
+		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				dispose();
+			}
+		});
+		contentPane.registerKeyboardAction(e -> dispose(), KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT); // call onCancel() on ESCAPE
+		//endregion
 
-        //region setBorder to Color.red
-        nicknameField.setBorder(new LineBorder(Color.red));
-        passwordField.setBorder(new LineBorder(Color.red));
-        //endregion
+		//region setBorder to Color.red
+		nicknameField.setBorder(new LineBorder(Color.red));
+		passwordField.setBorder(new LineBorder(Color.red));
+		//endregion
 
-        //region loginButton events
-        loginButton.addMouseListener(new MouseListener() {
-            public void mouseClicked(MouseEvent e) {
-            }
+		//region loginButton events
+		loginButton.addMouseListener(new MouseListener() {
+			public void mouseClicked(MouseEvent e) {
+			}
 
-            public void mousePressed(MouseEvent e) {
-            }
+			public void mousePressed(MouseEvent e) {
+			}
 
-            public void mouseReleased(MouseEvent e) {
-            }
+			public void mouseReleased(MouseEvent e) {
+			}
 
-            public void mouseEntered(MouseEvent e) {
-                loginButton.setEnabled(allFieldValid());
-            }
+			public void mouseEntered(MouseEvent e) {
+				loginButton.setEnabled(allFieldValid());
+			}
 
-            public void mouseExited(MouseEvent e) {
-            }
-        });
+			public void mouseExited(MouseEvent e) {
+			}
+		});
 		/*
 		loginButton.addActionListener(e -> {
 			// Getting data from the form
@@ -79,40 +79,40 @@ public class EatAdvisorLogin extends JDialog {
 			}
 		});
 		*/
-        //endregion
+		//endregion
 
-        registerButton.addActionListener(e -> EatAdvisorRegistration.main());
+		registerButton.addActionListener(e -> EatAdvisorRegistration.main());
 
-        //region Focus events
-        nicknameField.addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
-                nicknameField.selectAll();
-            }
+		//region Focus events
+		nicknameField.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				nicknameField.selectAll();
+			}
 
-            public void focusLost(FocusEvent e) {
-            }
-        });
-        nicknameField.addCaretListener(e -> {
-            Color color = Database.regexNickname(nicknameField.getText()) ? Color.green : Color.red;
-            nicknameField.setBorder(new LineBorder(color));
-        });
+			public void focusLost(FocusEvent e) {
+			}
+		});
+		nicknameField.addCaretListener(e -> {
+			Color color = Database.regexNickname(nicknameField.getText()) ? Color.green : Color.red;
+			nicknameField.setBorder(new LineBorder(color));
+		});
 
-        passwordField.addFocusListener(new FocusListener() {
-            public void focusGained(FocusEvent e) {
-                passwordField.selectAll();
-            }
+		passwordField.addFocusListener(new FocusListener() {
+			public void focusGained(FocusEvent e) {
+				passwordField.selectAll();
+			}
 
-            public void focusLost(FocusEvent e) {
-            }
-        });
-        passwordField.addCaretListener(e -> {
-            Color color = Database.regexPassword(String.valueOf(passwordField.getPassword())) ? Color.green : Color.red;
-            passwordField.setBorder(new LineBorder(color));
-        });
-        //endregion
-    }
+			public void focusLost(FocusEvent e) {
+			}
+		});
+		passwordField.addCaretListener(e -> {
+			Color color = Database.regexPassword(String.valueOf(passwordField.getPassword())) ? Color.green : Color.red;
+			passwordField.setBorder(new LineBorder(color));
+		});
+		//endregion
+	}
 
-    public static void main() {
+	public static void main() {
 		/*
 		EatAdvisorLogin dialog = new EatAdvisorLogin();
 		dialog.pack();
@@ -122,29 +122,27 @@ public class EatAdvisorLogin extends JDialog {
 		dialog.setVisible(true);
 		*/
 
-        String nickname = "ristoratore";
-        String password = "!Ristoratore1";
+		String nickname = "ristoratore";
+		String password = "!Ristoratore1";
 
-        try {
-            Customer customer = Database.getCustomer(nickname);
-            if (customer.nickname.equals(nickname) && customer.password.equals(password)) {
-                JOptionPane.showMessageDialog(null, "Welcome back!");
-                Profile.clt = customer;
-                Profile.main(true);
-            } else {
-                JOptionPane.showMessageDialog(null, "Customer not found");
-            }
-        } catch (IOException | ClassNotFoundException ioException) {
-            ioException.printStackTrace();
-        } catch (DatabaseExceptions dbExceptions) {
-            JOptionPane.showMessageDialog(null, "Customer not found");
-        }
-    }
+		try {
+			Customer customer = Database.getCustomer(nickname);
+			if (customer.nickname.equals(nickname) && customer.password.equals(password)) {
+				JOptionPane.showMessageDialog(null, "Welcome back!");
+				Profile.clt = customer;
+				Profile.main(true);
+			} else {
+				JOptionPane.showMessageDialog(null, "Customer not found");
+			}
+		} catch (IOException | ClassNotFoundException | DatabaseExceptions ioException) {
+			ioException.printStackTrace();
+		}
+	}
 
-    private boolean allFieldValid() {
-        Color[] colorArray = {((LineBorder) nicknameField.getBorder()).getLineColor(), ((LineBorder) passwordField.getBorder()).getLineColor()};
+	private boolean allFieldValid() {
+		Color[] colorArray = {((LineBorder) nicknameField.getBorder()).getLineColor(), ((LineBorder) passwordField.getBorder()).getLineColor()};
 
-        for (Color color : colorArray) if (color == Color.red) return false;
-        return true;
-    }
+		for (Color color : colorArray) if (color == Color.red) return false;
+		return true;
+	}
 }
